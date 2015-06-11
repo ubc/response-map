18b0e479b4c83cb988871076f467e0e1ce0d71c5
mwytang <michael.tang@ubc.ca>
2015-06-11 10:40:36 -0700

    FIXED invalid index error in OAuth
    - fixed the "Undefined index: query" error

--- OAuth.php
+++ OAuth.php
@@ -267,7 +267,7 @@ class OAuthRequest {
 
     // Parse the query-string to find and add GET parameters
     $parts = parse_url($http_url);
-    if ( $parts['query'] ) {
+    if ( !empty($parts['query']) ) {
       $qparms = OAuthUtil::parse_parameters($parts['query']);
       $parameters = array_merge($qparms, $parameters);
     }
