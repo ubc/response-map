f0c3c8b8f2e8fc81c454d3025a7fae66d144e46e
Pan Luo <pan.luo@ubc.ca>
2018-10-22 21:31:38 -0700

    Remove unused parameter from google map api

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 
 <!-- JAVASCRIPT -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//maps.googleapis.com/maps/api/js?v=3&key=<?php echo $config->google_key; ?>&sensor=false&libraries=geometry"></script>
+<script src="//maps.googleapis.com/maps/api/js?v=3&key=<?php echo $config->google_key; ?>&libraries=geometry"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js"></script>
 <script src="js/d3.layout.cloud.js"></script>
