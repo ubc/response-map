6b74a7cd62c8f40dd601a12c2a4196ef3d7971ca
Pan Luo <pan.luo@ubc.ca>
2018-10-22 21:06:47 -0700

    Fix google map api key and missing js

--- header.php
+++ header.php
@@ -7,11 +7,11 @@
 
 <!-- JAVASCRIPT -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//maps.googleapis.com/maps/api/js?v=3?sensor=false&libraries=geometry"></script>
+<script src="//maps.googleapis.com/maps/api/js?v=3?key=<?php echo $config->google_key; ?>&sensor=false&libraries=geometry"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js"></script>
 <script src="js/d3.layout.cloud.js"></script>
-<script src="//google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer_compiled.js"></script>
 <script src="js/jquery.ui.widget.js"></script>
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
 <script src="js/jquery.iframe-transport.js"></script>
